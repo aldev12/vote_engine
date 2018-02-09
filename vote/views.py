80bd65c26f1c81355c2080fa28675839a45b17ad
aldevkhv <novi12@bk.ru>
2018-02-09 18:29:43 +1000

    add vote

--- vote/views.py
+++ vote/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from vote.models import Competition, Participate, COMPETITION_TYPE
+from vote.models import Competition, Participate, COMPETITION_TYPE, Poll
 from django.http import Http404
 from .forms import CompetitionForm, ParticipateForm
 from django.utils import timezone
+from django.db import IntegrityError
 
 
 def competition_list(request):
@@ -53,7 +54,21 @@ def participate_add(request):
 
 
 def participates_in_competition(request):
+    message = None
     competition = request.GET.get('competition', None)
+    if request.user.is_authenticated:
+        participate = request.GET.get('vote', None)
+        if participate:
+            poll = Poll()
+            poll.user = request.user
+            try:
+                poll.participate = Participate.objects.get(title=participate)
+            except Participate.DoesNotExist:
+                pass
+            try:
+                poll.save()
+            except IntegrityError:
+                message = 'Вы уже голосовали за %s' % participate
     try:
         competition = Competition.objects.get(title=competition)
     except Competition.DoesNotExist:
@@ -72,5 +87,6 @@ def participates_in_competition(request):
 
     participates = competition.participates_competition.all()
     return render(request, "vote/participate.html", {'participates': participates, 'add_member': add_member,
-                                                     'competition': competition, 'vote_open': vote_open})
+                                                     'competition': competition, 'vote_open': vote_open,
+                                                     'message': message})
 
